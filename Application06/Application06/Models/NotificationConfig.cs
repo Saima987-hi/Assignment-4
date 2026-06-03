9030140df4ae264cc8763642156ba4ace1fa015e
Saima987-hi <247276@students.au.edu.pk>
2026-06-03 10:26:43 +0500

    Create Appication

---
+++ Application06/Application06/Models/NotificationConfig.cs
@@ -0,0 +1,8 @@
+namespace Application06.Models
+{
+    public class NotificationConfig
+    {
+        public int DefaultNumberOfNotifications { get; set; } = 5;
+        public string NotificationStyle { get; set; } = "Compact"; // "Compact" or "Detailed"
+    }
+}
