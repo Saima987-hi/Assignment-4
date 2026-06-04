ce38b850ee9c07f7b23a98b5496b15083a728622
Saima987-hi <247276@students.au.edu.pk>
2026-06-04 20:40:30 +0500

    Changing

---
+++ App8_TodoWithDatabase_fixed/SQLQuery2.sql
@@ -0,0 +1,12 @@
+﻿use ToDoDB;
+-- 1. TABLE BANAO
+CREATE TABLE TodoItems (
+    Id          INT           PRIMARY KEY IDENTITY(1,1),
+    Task        NVARCHAR(500) NOT NULL,
+    IsCompleted INT           NOT NULL DEFAULT 0,
+    CreatedAt   NVARCHAR(100) NOT NULL
+);
+
+-- 2. SAB ITEMS DEKHO
+SELECT*FROM TodoItems
+ORDER BY Id DESC;
